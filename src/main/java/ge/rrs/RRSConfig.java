72a774405d43aa0fa55a50cc3bbc2ed99f90a464
Giorgi Butbaia <giorgi.butbaia@toptal.com>
2020-07-20 17:49:44 +0400

    Configured WebMVC, moved login.jsp and updated .gitignore

--- src/main/java/ge/rrs/RRSConfig.java
+++ src/main/java/ge/rrs/RRSConfig.java
@@ -1,17 +1,27 @@
 package ge.rrs;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+// MVC
+import org.springframework.web.servlet.config.annotation.*;
 
 /**
  * Configuration class for Spring framework.
  * Takes the responsibility of rrs-servlet.xml configuration file.
  */
 @Configuration
+@EnableWebMvc
 @ComponentScan({"ge.rrs"})
-public class RRSConfig {
+public class RRSConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+
+        // Auth views
+		registry.addViewController("/login").setViewName("login");
+	}
 
     /**
      * Filenames returned by Spring controllers will be suffixed with '.jsp'.
