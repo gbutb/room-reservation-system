fa5d8354a5cd47ebc0e4de3e20009275898b5e3e
dadddda <daduka9k@gmail.com>
2020-07-22 03:41:32 +0400

    Added webapp resources folder to Spring MVC so it can be accessed from .jsp/.html files

--- src/main/java/ge/rrs/RRSConfig.java
+++ src/main/java/ge/rrs/RRSConfig.java
@@ -3,6 +3,9 @@ package ge.rrs;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 /**
@@ -10,8 +13,19 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
  * Takes the responsibility of rrs-servlet.xml configuration file.
  */
 @Configuration
+@EnableWebMvc
 @ComponentScan({"ge.rrs"})
-public class RRSConfig {
+public class RRSConfig implements WebMvcConfigurer {
+
+    /**
+     * Adds webapp resources folder to Spring MVC so it can be accessed from .jsp/.html files.
+     *
+     * @param registry registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("resources/**").addResourceLocations("/resources/");
+    }
 
     /**
      * Filenames returned by Spring controllers will be suffixed with '.jsp'.
