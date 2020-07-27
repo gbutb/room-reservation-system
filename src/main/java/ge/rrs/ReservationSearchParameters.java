979c7c1bf381fdcf2e3fc70ac247347f9bb8dd6d
shavtvalishvili <gshavtvalishvili@gmail.com>
2020-07-27 19:54:15 +0400

    Added initial implementation of ReservationSearchParameters methods

--- src/main/java/ge/rrs/ReservationSearchParameters.java
+++ src/main/java/ge/rrs/ReservationSearchParameters.java
@@ -1,15 +1,34 @@
 package ge.rrs;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class ReservationSearchParameters implements SearchParameters {
+
+    private Collection<SearchParameter> parameters;
+
+    public ReservationSearchParameters() {
+        parameters = new ArrayList<>();
+    }
+
     @Override
     public void addParameter(SearchParameter parameter) {
+        parameters.add(parameter);
+    }
 
+    public void addDateTimeRangeParameter(String dateFrom, String dateTo) {
+        parameters.add(ReservationSearchParameter.startsAfter(dateFrom));
+        parameters.add(ReservationSearchParameter.endsBefore(dateTo));
+    }
+
+    public void addRepeatedInTimeRangeParameter(String timeFrom, String timeTo) {
+        parameters.add(ReservationSearchParameter.startsAfterTime(timeFrom));
+        parameters.add(ReservationSearchParameter.endsBeforeTime(timeTo));
+        parameters.add(ReservationSearchParameter.isRepeated(true));
     }
 
     @Override
     public Collection<SearchParameter> getParameters() {
-        return null;
+        return parameters;
     }
 }
