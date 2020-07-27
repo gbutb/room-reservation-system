470f633b77bd3bd6cedb1ec6d03a8abe03b07514
shavtvalishvili <gshavtvalishvili@gmail.com>
2020-07-27 19:57:01 +0400

    - Minor code reformat - Added renderData instance variable in Room.java

--- src/main/java/ge/rrs/Room.java
+++ src/main/java/ge/rrs/Room.java
@@ -2,7 +2,7 @@ package ge.rrs;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class Room extends TableEntry {
@@ -19,47 +19,53 @@ public class Room extends TableEntry {
     private int commentId;
     private boolean conditioner;
     private boolean projector;
+    private String renderData;
 
-    Room(int roomId) {
+    public Room(int roomId) {
         // TODO: implement this;
     }
 
-    Room(int roomId, int roomSize, int floor, int commentId,
-         boolean conditioner, boolean projector, DBConnection connection) {
-        this.roomId =  roomId;
+    public Room(int roomId, int roomSize, int floor, int commentId,
+                boolean conditioner, boolean projector, String renderData, DBConnection connection) {
+        this.roomId = roomId;
         this.roomSize = roomSize;
         this.floor = floor;
         this.commentId = commentId;
         this.conditioner = conditioner;
         this.projector = projector;
+        this.renderData = renderData;
 
         this.connection = connection;
     }
 
     /**
      * Null Constructor.
+     *
      * @param connection A reference to DB connection.
      */
-    Room(DBConnection connection) {
+    public Room(DBConnection connection) {
         this.connection = connection;
     }
 
     @Override
-    public DBConnection getConnection() { return connection; }
+    public DBConnection getConnection() {
+        return connection;
+    }
 
     @Override
     public Collection<? extends TableEntry> fromResultSet(ResultSet rs) throws SQLException {
-        Collection<Room> entries = new ArrayDeque<>();
+        Collection<Room> entries = new ArrayList<>();
         while (rs.next()) {
             // Add new entry
             entries.add(new Room(
-                rs.getInt("room_id"),
-                rs.getInt("room_size"),
-                rs.getInt("floor"),
-                rs.getInt("comment_id"),
-                rs.getBoolean("conditioner"),
-                rs.getBoolean("projector"),
-                getConnection()
+                    rs.getInt("room_id"),
+                    rs.getInt("room_size"),
+                    rs.getInt("floor"),
+                    rs.getInt("comment_id"),
+                    rs.getBoolean("conditioner"),
+                    rs.getBoolean("projector"),
+                    rs.getString("render_data"),
+                    getConnection()
             ));
         }
         return entries;
@@ -108,5 +114,9 @@ public class Room extends TableEntry {
 
     public boolean isProjector() {
         return projector;
+    }
+
+    public String getRenderData() {
+        return renderData;
     }
 }
--- src/main/java/ge/rrs/RoomSearchParameter.java
+++ src/main/java/ge/rrs/RoomSearchParameter.java
@@ -10,10 +10,11 @@ public class RoomSearchParameter implements SearchParameter {
 
     /**
      * Initializes Room Search parameter.
-     * @param key The key of the query.
-     * @param relation Relation between the key and value.
+     *
+     * @param key             The key of the query.
+     * @param relation        Relation between the key and value.
      * @param valueExpression Expression, with arguments replaced with '?'
-     * @param args Arguments.
+     * @param args            Arguments.
      */
     RoomSearchParameter(String key, String relation, String valueExpression, String[] args) {
         this.key = key;
@@ -25,20 +26,24 @@ public class RoomSearchParameter implements SearchParameter {
     /**
      * Creates a Room search parameter between
      * start floor and the end floor.
-     * @param start: Initial floor.
-     * @param end: Final floor.
-     * @return: Room Search Parameter.
+     *
+     * @param start Initial floor.
+     * @param end   Final floor.
+     * @return Room Search Parameter.
      */
     static RoomSearchParameter fromFloorRange(int start, int end) {
-        RoomSearchParameter param = new RoomSearchParameter(
-            "floor", " BETWEEN ", "? AND ?", new String[] {
-                ""+start, ""+end});
-        return param;
+        return new RoomSearchParameter(
+                "floor", " BETWEEN ", "? AND ?", new String[]{
+                "" + start, "" + end});
     }
 
     static RoomSearchParameter fromDateTimeRange(String dateFrom, String dateTo) {
+        ReservationSearchParameters rParams = new ReservationSearchParameters();
+        rParams.addDateTimeRangeParameter(dateFrom, dateTo);
+//        Reservation r = new Reservation(); // TODO change this
+//        r.filter(rParams.getParameters());
 
-        return new RoomSearchParameter("", "", "", new String[] {});
+        return new RoomSearchParameter("room_id", " IN ", "()", new String[]{});
     }
 
     @Override
@@ -49,7 +54,7 @@ public class RoomSearchParameter implements SearchParameter {
     @Override
     public String getValue() throws Exception {
         return String.format(
-            valueExpression.replace("?", "%s"), (Object[])args);
+                valueExpression.replace("?", "%s"), (Object) args);
     }
 
     @Override
