1a72b026f89ca446c29579d40256be7a1760a566
shavtvalishvili <gshavtvalishvili@gmail.com>
2020-07-28 03:02:32 +0400

    Reconstructed ReservationSearchParameters.java according to updated SearchParameters interface

--- src/main/java/ge/rrs/ReservationSearchParameters.java
+++ src/main/java/ge/rrs/ReservationSearchParameters.java
@@ -1,34 +1,105 @@
 package ge.rrs;
 
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.List;
 
 public class ReservationSearchParameters implements SearchParameters {
 
-    private Collection<SearchParameter> parameters;
+    // Data structure to construct a statement from all user given parameters
+    private final StringBuilder parameters;
+    // Data Structure to save all parameters' value expression's arguments
+    List<String> arguments;
 
     public ReservationSearchParameters() {
-        parameters = new ArrayList<>();
+        parameters = new StringBuilder();
+        arguments = new ArrayList<>();
     }
 
     @Override
-    public void addParameter(SearchParameter parameter) {
-        parameters.add(parameter);
-    }
+    public void addParameter(SearchParameter parameter) throws Exception {
+        if (parameters.length() != 0) parameters.append(" AND ");
+        parameters.append(parameter.getKey());
+        parameters.append(parameter.getRelation());
+        parameters.append(parameter.getValueExpression());
 
-    public void addDateTimeRangeParameter(String dateFrom, String dateTo) {
-        parameters.add(ReservationSearchParameter.startsAfter(dateFrom));
-        parameters.add(ReservationSearchParameter.endsBefore(dateTo));
-    }
-
-    public void addRepeatedInTimeRangeParameter(String timeFrom, String timeTo) {
-        parameters.add(ReservationSearchParameter.startsAfterTime(timeFrom));
-        parameters.add(ReservationSearchParameter.endsBeforeTime(timeTo));
-        parameters.add(ReservationSearchParameter.isRepeated(true));
+        arguments.addAll(parameter.getValueArgs());
     }
 
     @Override
-    public Collection<SearchParameter> getParameters() {
-        return parameters;
+    public void addParameter(String operator, SearchParameter parameter) throws Exception {
+        if (parameters.length() != 0) parameters.append(operator);
+        parameters.append(parameter.getKey());
+        parameters.append(parameter.getRelation());
+        parameters.append(parameter.getValueExpression());
+
+        arguments.addAll(parameter.getValueArgs());
+    }
+
+    @Override
+    public void addParametersClause(SearchParameter... inputParameters) throws Exception {
+        if (parameters.length() != 0) parameters.append(" AND ");
+        parameters.append("( ");
+        for (SearchParameter parameter : inputParameters) {
+            parameters.append(parameter.getKey());
+            parameters.append(parameter.getRelation());
+            parameters.append(parameter.getValueExpression());
+
+            arguments.addAll(parameter.getValueArgs());
+        }
+        parameters.append(" )");
+    }
+
+    @Override
+    public void addParametersClause(String operator, SearchParameter... inputParameters) throws Exception {
+        if (parameters.length() != 0) parameters.append(operator);
+        parameters.append("( ");
+        for (SearchParameter parameter : inputParameters) {
+            parameters.append(parameter.getKey());
+            parameters.append(parameter.getRelation());
+            parameters.append(parameter.getValueExpression());
+
+            arguments.addAll(parameter.getValueArgs());
+        }
+        parameters.append(" )");
+    }
+
+    @Override
+    public String getParametersStatement() {
+        return parameters.toString();
+    }
+
+    @Override
+    public List<String> getArguments() {
+        return arguments;
+    }
+
+    /**
+     * Adds parameters, which serve fetching reservations
+     * which overlap the user given exact date-time range
+     *
+     * @param dateFrom start of the date-time interval
+     * @param dateTo   end of the date-time interval
+     */
+    public void addDateTimeRangeOverlapParameter(String dateFrom, String dateTo) throws Exception {
+        addParametersClause(ReservationSearchParameter.startsBefore(dateFrom, true),
+                ReservationSearchParameter.endsAfter(dateFrom, false));
+        addParametersClause(" OR ", ReservationSearchParameter.startsBefore(dateTo, false),
+                ReservationSearchParameter.endsAfter(dateTo, true));
+
+    }
+
+    /**
+     * Adds parameters, which serve fetching repeated reservations
+     * which overlap the user given time range (only hours and minutes are considered)
+     *
+     * @param timeFrom start of the time interval
+     * @param timeTo   end of the time interval
+     */
+    public void addRepeatAndTimeRangeOverlapParameter(String timeFrom, String timeTo) throws Exception {
+        addParameter(ReservationSearchParameter.isRepeated(true));
+        addParametersClause(ReservationSearchParameter.startsBeforeTime(timeFrom, true),
+                ReservationSearchParameter.endsAfterTime(timeFrom, false));
+        addParametersClause(" OR ", ReservationSearchParameter.startsBeforeTime(timeTo, false),
+                ReservationSearchParameter.endsAfterTime(timeTo, true));
     }
 }
