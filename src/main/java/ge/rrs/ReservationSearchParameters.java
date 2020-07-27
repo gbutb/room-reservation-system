e260aeed30064ad2bfcd36426e0264b0e5129181
shavtvalishvili <gshavtvalishvili@gmail.com>
2020-07-27 16:56:39 +0400

    added comments to SearchParameters

---
+++ src/main/java/ge/rrs/ReservationSearchParameters.java
@@ -0,0 +1,15 @@
+package ge.rrs;
+
+import java.util.Collection;
+
+public class ReservationSearchParameters implements SearchParameters {
+    @Override
+    public void addParameter(SearchParameter parameter) {
+
+    }
+
+    @Override
+    public Collection<SearchParameter> getParameters() {
+        return null;
+    }
+}
--- src/main/java/ge/rrs/SearchParameters.java
+++ src/main/java/ge/rrs/SearchParameters.java
@@ -4,7 +4,15 @@ import java.util.Collection;
 
 public interface SearchParameters {
 
+    /**
+     * Adds given parameter to all saved parameters
+     * @param parameter user given parameter
+     */
     void addParameter(SearchParameter parameter);
 
+    /**
+     * Returns a complete collection of user added parameters
+     * @return returns a complete collection of user added parameters
+     */
     Collection<SearchParameter> getParameters();
 }
