fc22943afecdf725d8e9d311bb503169772132eb
dadddda <62848572+dadddda@users.noreply.github.com>
2020-07-27 16:14:04 +0400

    Revert "Develop"

--- Readme.md
+++ Readme.md
@@ -1,18 +1,2 @@
-# Room Reservation System 
+# Room Reservation system 
 [![Actions Status](https://github.com/gbutb/room-reservation-system/workflows/MavenTest/badge.svg)](https://github.com/gbutb/room-reservation-system/actions)
-
-## Build instructions
-`cd` to the root of the repository and execute:
-```zsh
-> mvn clean package
-``` 
-To run tests and generate coverage report use:
-```zsh
-> mvn clean verify
-```
-
-## Deploying
-To deploy the application run the following command:
-```zsh
-> mvn clean spring-boot:run 
-```
--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -1,9 +1,9 @@
 USE reservations_db;
 
 DROP TABLE IF EXISTS reservations;
-DROP TABLE IF EXISTS rooms;
+DROP TABLE IF EXISTS room;
 DROP TABLE IF EXISTS room_comment;
-DROP TABLE IF EXISTS accounts;
+DROP TABLE IF EXISTS account;
 
 CREATE TABLE room_comment (
 	comment_id INT PRIMARY KEY,
@@ -11,7 +11,7 @@ CREATE TABLE room_comment (
 	user_comment VARCHAR(1000)
 );
 
-CREATE TABLE rooms (
+CREATE TABLE room (
 	room_id INT PRIMARY KEY,
 	room_size INT,
 	floor INT,
@@ -21,11 +21,9 @@ CREATE TABLE rooms (
 	FOREIGN KEY (comment_id) REFERENCES room_comment(comment_id)
 );
 
-CREATE TABLE accounts (
+CREATE TABLE account (
 	account_id INT PRIMARY KEY,
-	username VARCHAR(255),
-	encryptedPassword VARCHAR(255),
-	email VARCHAR(255)
+	username VARCHAR(25)
 );
 
 CREATE TABLE reservations (
@@ -35,20 +33,18 @@ CREATE TABLE reservations (
 	end_date DATETIME,
 	do_repeat BOOLEAN,
 	account_id INT,
-	FOREIGN KEY (room_id) REFERENCES rooms(room_id),
-	FOREIGN KEY (account_id) REFERENCES accounts(account_id)
+	FOREIGN KEY (room_id) REFERENCES room(room_id),
+	FOREIGN KEY (account_id) REFERENCES account(account_id)
 );
 	
 INSERT INTO room_comment VALUES
 	(0, STR_TO_DATE("9,7,2020 14,30,0", "%d,%m,%Y %H,%i,%s"), "Hello room!");
 
-INSERT INTO rooms VALUES
-	(200, 25, 2, true, true, 0),
-	(315, 20, 3, false, true, 0);
+INSERT INTO room VALUES
+	(200, 25, 2, true, true, 0);
 	
-INSERT INTO accounts VALUES
-	(0, "Human", "human_password", "human@humans.org"),
-	(1, "Human 1", "human_1_password", "human1@humans.org");
+INSERT INTO account VALUES
+	(0, "Human");
 	
 INSERT INTO reservations VALUES
 	(0, 200, STR_TO_DATE("10,7,2020 14,40,0", "%d,%m,%Y %H,%i,%s"), 
