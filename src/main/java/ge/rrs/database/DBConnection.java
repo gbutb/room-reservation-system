36da6a3d1a7c44321cad637a623eff6b6f3d603d
dadddda <daduka9k@gmail.com>
2020-07-31 15:03:59 +0400

    Added executeSQLFrom method to DBConnection.java with minor code reformat

--- src/main/java/ge/rrs/database/DBConnection.java
+++ src/main/java/ge/rrs/database/DBConnection.java
@@ -2,10 +2,17 @@
 package ge.rrs.database;
 
 
+import org.apache.ibatis.jdbc.ScriptRunner;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.Reader;
 import java.sql.*;
 import java.util.List;
 
 public class DBConnection {
+
     private static final String SERVER = "localhost";
     private static final String USER = "root";
     private static final String PASSWORD = "12345678";
@@ -28,23 +35,9 @@ public class DBConnection {
         statement.executeQuery("USE " + db_name);
     }
 
-
-    /**
-     *If connection doesn't exist, new connection is created.
-     * @return DBConnection
-     * @throws SQLException
-     */
-    public static DBConnection getDBConnection() throws SQLException {
-
-        if (dbConnection == null){
-            dbConnection = new DBConnection();
-        }
-        return dbConnection;
-    }
-
-
     /**
      * makes database connection
+     *
      * @throws SQLException
      */
     private void connect() throws SQLException {
@@ -53,44 +46,70 @@ public class DBConnection {
         statement.executeQuery("USE " + DB_NAME);
     }
 
+    /**
+     * If connection doesn't exist, new connection is created.
+     *
+     * @return DBConnection
+     * @throws SQLException
+     */
+    public static DBConnection getDBConnection() throws SQLException {
+
+        if (dbConnection == null) {
+            dbConnection = new DBConnection();
+        }
+        return dbConnection;
+    }
 
     /**
      * closes connection
+     *
      * @throws SQLException
      */
     public void closeConnection() throws SQLException {
         connection.close();
     }
 
-
     /**
      * executes query
+     *
      * @param command - sql command
-     * @param args - arguments for prepared statement
-     * @throws SQLException
+     * @param args    - arguments for prepared statement
      * @return result set
+     * @throws SQLException
      */
     public ResultSet executeQuery(String command, List<String> args) throws SQLException {
         PreparedStatement ps = connection.prepareStatement(command);
         for (int i = 0; i < args.size(); i++) {
-            ps.setString(i+1, args.get(i));
+            ps.setString(i + 1, args.get(i));
         }
         return ps.executeQuery();
     }
 
-
-
     /**
      * executes update
+     *
      * @param command - sql command
-     * @param args - arguments for prepared statement
+     * @param args    - arguments for prepared statement
      * @throws SQLException
      */
-    public void executeUpdate(String command, List<String> args)  throws SQLException {
+    public void executeUpdate(String command, List<String> args) throws SQLException {
         PreparedStatement ps = connection.prepareStatement(command);
         for (int i = 0; i < args.size(); i++) {
-            ps.setString(i+1, args.get(i));
+            ps.setString(i + 1, args.get(i));
         }
         ps.executeUpdate();
+    }
+
+    /**
+     * Reads given .sql file and executes contained commands.
+     *
+     * @param filename .sql file
+     * @throws FileNotFoundException file doesn't exist or is inaccessible
+     */
+    public void executeSQLFrom(String filename) throws FileNotFoundException {
+        ScriptRunner scriptRunner = new ScriptRunner(connection);
+        Reader reader = new BufferedReader(new FileReader(filename));
+
+        scriptRunner.runScript(reader);
     }
 }
