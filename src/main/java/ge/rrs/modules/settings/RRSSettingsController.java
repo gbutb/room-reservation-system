8effa28c74221918e1ad9e973c051b8cb1c1888d
MariamChilaia <mchil20@freeuni.edu.ge>
2020-07-31 18:04:56 +0400

    controller of settings

--- src/main/java/ge/rrs/modules/settings/RRSSettingsController.java
+++ src/main/java/ge/rrs/modules/settings/RRSSettingsController.java
@@ -3,17 +3,28 @@ package ge.rrs.modules.settings;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import ge.rrs.modules.auth.RRSUser;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class RRSSettingsController {
-
     @RequestMapping("/settings")
-    public ModelAndView settingsView() {
+    public ModelAndView settingsView(HttpServletRequest req) {
         ModelAndView mv = new ModelAndView();
+        if (req.getParameter("afterSave") != null) {
+            RRSUser user = RRSUser.getCurrentUser();
+            user.setPassword(req.getParameter("password"));
+            user.setPhoneNumber(req.getParameter("number"));
+            try{
+                user.updateEntry();
+            }catch(Exception e) {
 
-        // TODO: User code here
-
-        mv.setViewName("/settings/settings");
+            }
+            mv.setViewName("/homepage/homepage-graphical-view");
+        } else {
+            mv.setViewName("/settings/settings");
+        }
         return mv;
     }
 }
