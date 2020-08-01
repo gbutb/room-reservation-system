af8b84a75a10b9a8694e58e6e19d9aa0f36bed9b
dadddda <daduka9k@gmail.com>
2020-08-01 19:32:58 +0400

    Added RoomSearchParameter initialization in RRSSessionListener.java

--- src/main/java/ge/rrs/RRSSessionListener.java
+++ src/main/java/ge/rrs/RRSSessionListener.java
@@ -1,6 +1,8 @@
 // RRSSessionListener.java
 package ge.rrs;
 
+import ge.rrs.database.room.RoomSearchParameters;
+
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
@@ -13,12 +15,19 @@ public class RRSSessionListener implements HttpSessionListener {
     @Override
     public void sessionCreated(HttpSessionEvent se) {
         HttpSession session = se.getSession();
+
+        RoomSearchParameters roomSearchParameters = new RoomSearchParameters();
+        session.setAttribute("filterParams", roomSearchParameters);
+
         System.out.println("session initialized");
     }
 
     @Override
     public void sessionDestroyed(HttpSessionEvent se) {
         HttpSession session = se.getSession();
+
+        session.removeAttribute("filterParams");
+
         System.out.println("session destroyed");
     }
 }
