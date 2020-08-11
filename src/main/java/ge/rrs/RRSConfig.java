d2ed9197f9ffb3fc6aaf5578028236d38eceadd8
gbutb <gbutb17@freeuni.edu.ge>
2020-08-11 17:20:47 +0400

    Configured redirects to SSL

--- src/main/java/ge/rrs/RRSConfig.java
+++ src/main/java/ge/rrs/RRSConfig.java
@@ -1,6 +1,15 @@
 // RRSConfig.java
 package ge.rrs;
 
+// Apache
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+
+// Spring
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -10,16 +19,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 /**
- * Configuration class for Spring framework.
- * Takes the responsibility of rrs-servlet.xml configuration file.
+ * Configuration class for Spring framework. Takes the responsibility of
+ * rrs-servlet.xml configuration file.
  */
 @Configuration
 @EnableWebMvc
-@ComponentScan({"ge.rrs"})
+@ComponentScan({ "ge.rrs" })
 public class RRSConfig implements WebMvcConfigurer {
 
     /**
-     * Adds webapp resources folder to Spring MVC so it can be accessed from .jsp/.html files.
+     * Adds webapp resources folder to Spring MVC so it can be accessed from
+     * .jsp/.html files.
      *
      * @param registry registry
      */
@@ -40,5 +50,36 @@ public class RRSConfig implements WebMvcConfigurer {
         vr.setSuffix(".jsp");
 
         return vr;
+    }
+
+    ///////////////////////////
+    // Sets up SSL redirects //
+    ///////////////////////////
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            public void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(getConnector());
+        return tomcat;
+    }
+
+    private Connector getConnector() {
+        Connector connector = new Connector(
+            TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(8443);
+        return connector;
     }
 }
